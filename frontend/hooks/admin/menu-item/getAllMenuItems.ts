0a49d3716dc2f91d58913b540f7cff3f4778b015
gautamaswin20-hub <gautamaswin20@gmail.com>
2026-07-20 02:40:29 +0545

    Update code changes

--- frontend/hooks/admin/menu-item/getAllMenuItems.ts
+++ frontend/hooks/admin/menu-item/getAllMenuItems.ts
@@ -4,7 +4,7 @@ import { menuItemApi } from "@/lib/api/menu-item.api";
 import { UsePaginationParams } from "@/lib/types/usePagination";
 import { useQuery } from "@tanstack/react-query";
 
-export function useAllMenuItems({ page = 1, limit = 10, search }: UsePaginationParams) {
+export function useAllMenuItems({ page = 1, limit = 1000, search }: UsePaginationParams) {
   return useQuery({
     queryKey: ["menu-items", page, limit, search],
     queryFn: () => menuItemApi.getAllMenuItemApi({page, limit, search}),  
